e8248ecb340a3b96804a59f0a23605897345534f
d1n8 <mr.dinyar@mail.ru>
2025-11-23 09:01:09 +0500

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -132,7 +132,7 @@ export default function Header() {
               <Link to="/library/events" className="mobile-nav-item" onClick={() => setIsMenuOpen(false)}>Мероприятия</Link>
               <Link to="/library/issued" className="mobile-nav-item" onClick={() => setIsMenuOpen(false)}>Выданные книги</Link>
               <Link to="/library/new-book" className="mobile-nav-item" onClick={handleOpenModal}>Добавить книгу</Link>
-              <Link to="/library/reg-reader" className="mobile-nav-item" onClick={() => setIsMenuOpen(false)}>Регистрация читателя</Link>
+              <Link to="/library/register-reader" className="mobile-nav-item" onClick={() => setIsMenuOpen(false)}>Регистрация читателя</Link>
               <Link to="/library/renew" className="mobile-nav-item" onClick={() => setIsMenuOpen(false)}>Заявки на продление</Link>
             </>
           )}
