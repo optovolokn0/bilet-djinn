9294f7d19d5d2d536e620c844916836cc4d0bbe9
Georgy Kabitsky <georgijkabickij1@gmail.com>
2025-11-23 10:02:07 +0500

    update

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -65,13 +65,13 @@ export default function Header() {
 
   return (
     <header className="header">
-      <div className="burger-btn" onClick={toggleMenu}>
-        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      
+        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={toggleMenu} className='burger'>
           <path d="M4 18L20 18" stroke="white" strokeWidth="2" strokeLinecap="round" />
           <path d="M4 12L20 12" stroke="white" strokeWidth="2" strokeLinecap="round" />
           <path d="M4 6L20 6" stroke="white" strokeWidth="2" strokeLinecap="round" />
         </svg>
-      </div>
+     
 
       <Link to="/" className="logo">Библиотеки Екатеринбурга</Link>
 
